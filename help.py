deec2454e5ae0f11849a3887ccaff6c0923fea1e
RossTheRoss <msattr@gmail.com>
2019-01-17 15:32:09 -0600

    Add actual glorious FizzBuzz

--- help.py
+++ help.py
@@ -1,1 +1,12 @@
-#  Test
+#  Time to make a real Fizzbuzz
+for e in range (0,101):
+    string = ''
+    if e % 3 == 0:
+        string = string + "Fizz"
+    if e % 5 == 0:
+            string = string + "Buzz"
+    if e % 5 != 0 and e % 3 != 0:
+        print(e)
+    else:
+        print(string)
+        
