258f73ddfc983af66e35e60e51599079d3a143a1
Matthew Strapp <msattr@gmail.com>
2019-01-22 10:41:53 -0600

    Add week 0 file

---
+++ wk0/lab0.cpp
@@ -0,0 +1,12 @@
+// Lab 0
+// Matthew Strapp
+// 2019-01-23
+//
+// This program displays a message to the screen
+#include <iostream> using namespace std;
+
+int main()
+{
+cout << "Hello!" << endl; return 0;
+}
+
