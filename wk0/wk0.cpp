cfe0930bc91bec0ef62b1adb25e28173ea5626a0
Matthew Strapp <msattr@gmail.com>
2019-01-22 19:48:18 +0000

    Fix?

---
+++ wk0/wk0.cpp
@@ -0,0 +1,11 @@
+// Lab 0
+// Matthew Strapp
+// 2018-01-23
+//
+// This program displays a message to the screen
+#include <iostream> using namespace std;
+
+int main()
+{
+cout << "Hello!" << endl; return 0;
+}
