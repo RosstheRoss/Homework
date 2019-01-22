628888dd61ed5c7f98d95e88a11e4b563224a75b
Matthew Strapp <msattr@gmail.com>
2019-01-22 13:58:26 -0600

    AHHHHHHHHHHHHHH

--- wk0/lab0.cpp
+++
@@ -1,5 +0,0 @@
-// Lab 0
-// Matthew Strapp
-// 2019-01-23
-//
-// This program displays a message to the screen
--- wk0/wk0.cpp
+++ wk0/wk0.cpp
@@ -3,9 +3,11 @@
 // 2018-01-23
 //
 // This program displays a message to the screen
-#include <iostream> using namespace std;
+#include <iostream>
+using namespace std;
 
 int main()
 {
-cout << "Hello!" << endl; return 0;
+cout << "Hello!" << endl;
+return 0;
 }
