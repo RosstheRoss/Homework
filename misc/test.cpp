96e61378343e7b84d33774a09be9fc94aa66a3be
RossTheRoss <msattr@gmail.com>
2019-02-07 21:36:19 +0000

    Bored in Lecture? Find prime numbers!

---
+++ misc/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main () {
+  unsigned long long x;
+  int a;
+  cout << "Enter a number:";
+  cin >> x;
+  unsigned long long i = (x-1);
+  
+  for (i=x; i>1; --i/*--*/) {
+    if (x%i==0 && x!=i) {
+      cout << "Not prime!" << endl;
+      a=1700;
+      break;
+      return 3;
+    }
+  }
+  
+  if (a!=1700)
+  cout << "Prime!" << endl;
+  return 0;
+}
