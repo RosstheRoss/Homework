4fbb02dbbb316c02ca74b74995d692224aa17cf8
RossTheRoss <msattr@gmail.com>
2019-02-07 18:56:33 +0000

    Fix stupid mistake

--- ee1301/wk1/lab1/strap012_lab1_c_1.cpp
+++ ee1301/wk1/lab1/strap012_lab1_c_1.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main()
 {
-    string name1, name2
-    int h1, h2
+    string name1, name2;
+    int h1, h2;
     
-    cout << "Enter your name: ",
-    cin >> name1,
-    cout << "Enter your height: ",
-    cin >> h1,
-    cout << "Enter your comrade's name: ",
-    cin >> name2,
-    cout << "Enter your comrade's height: ",
-    cin >> h2,
+    cout << "Enter your name: ";
+    cin >> name1;
+    cout << "Enter your height: ";
+    cin >> h1;
+    cout << "Enter your comrade's name: ";
+    cin >> name2;
+    cout << "Enter your comrade's height: ";
+    cin >> h2;
     
-    cout << name1 << " and " << name2 << " standing on top of each other are " << h1+h2 << " feet tall!"
+    cout << name1 << " and " << name2 << " standing on top of each other are " << h1+h2 << " feet tall!";
   
   
 }
