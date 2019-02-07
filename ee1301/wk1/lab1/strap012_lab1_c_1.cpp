ef17791fa521d313f34766a7d9c6ebeea0c63ae9
RossTheRoss <msattr@gmail.com>
2019-02-07 19:04:03 +0000

    Fix dumb things again

--- ee1301/wk1/lab1/strap012_lab1_c_1.cpp
+++ ee1301/wk1/lab1/strap012_lab1_c_1.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    string name1, name2;
+    string name1;
+    string name2;
     int h1, h2;
     
     cout << "Enter your name: ";
-    cin >> name1;
+    getline (cin,name1);
     cout << "Enter your height: ";
     cin >> h1;
     cout << "Enter your comrade's name: ";
-    cin >> name2;
+    cin.ignore();
+    getline (cin,name2);
     cout << "Enter your comrade's height: ";
     cin >> h2;
     
-    cout << name1 << " and " << name2 << " standing on top of each other are " << h1+h2 << " feet tall!";
+    cout << name1 << " and " << name2 << " standing on top of each other are " << h1+h2 << " feet tall!" << endl;
   
   
 }
