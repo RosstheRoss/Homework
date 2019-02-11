f99d7e24a3d36adbc1ea831bb6905d75b994b355
RossTheRoss <msattr@gmail.com>
2019-02-11 20:25:35 +0000

    Fix

--- misc/test.cpp
+++ misc/test.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main () {
   unsigned long long x;
-  int a;
   cout << "Enter a number:";
   cin >> x;
   unsigned long long i = (x-1);
-  
+  int a;
   for (i=x; i>1; --i) {
     if (x%i==0 && x!=i) {
       cout << "Not prime!" << endl;
-      a=1700;
+      a=1;
       break;
       return 3;
     }
   }
   
-  if (a!=1700)
+  if (a!=1)
   cout << "Prime!" << endl;
   return 2;
 }
