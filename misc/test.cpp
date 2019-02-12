fca75858fff65231226326cd4201ecec203a591e
RossTheRoss <msattr@gmail.com>
2019-02-12 20:39:40 +0000

    Parenthesis are hard

--- misc/test.cpp
+++ misc/test.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include <iomanip>
-#include <math.h>
 using namespace std;
 
 int main () {
-  unsigned long long x;
-  cout << "Enter a number:";
-  cin >> x;
-  unsigned long long i = (x-1);
+  unsigned long long x,i;
   int a;
-  for (i=x; i>1; --i) {
-    if (x%i==0 && x!=i) {
+  cout << "Enter a number:"; cin >> x;
+  
+  for (i=x; i>1; --i)
+    
+    {if (x%i==0 && x!=i) {
       cout << "Not prime!" << endl;
       a=1;
       break;
-      return i;
-    }
-  }
+      return i;}}
   
   if (a!=1)
   cout << "Prime!" << endl;
