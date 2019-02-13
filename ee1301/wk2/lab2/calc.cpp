6802e55fced7a6954df7767f3db6dd8141102582
Matthew Strapp <msattr@gmail.com>
2019-02-13 13:41:51 -0600

    Doing fixes before lab

--- ee1301/wk2/lab2/calc.cpp
+++ ee1301/wk2/lab2/calc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main () {
   int a,b;
   char op;
-  cout << "Enter an equation: "
-  cin >> a, op, b;
-  cout << a << op << b << "="
+  cout << "Enter an equation: ";
+  cin >> a >> op >> b;
+  cout << a << op << b << " = " <<  << endl;
 }
