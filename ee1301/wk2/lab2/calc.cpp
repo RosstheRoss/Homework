3299e23e5e18f77d0d93fa345809787333b91c89
Matthew Strapp <msattr@gmail.com>
2019-02-13 13:57:59 -0600

    Finish calculator

--- ee1301/wk2/lab2/calc.cpp
+++ ee1301/wk2/lab2/calc.cpp
@@ -6,5 +6,18 @@ int main () {
   char op;
   cout << "Enter an equation: ";
   cin >> a >> op >> b;
-  cout << a << op << b << " = " <<  << endl;
-}
+  cout << a << op << b << " = ";
+   if (op=='+'){
+     cout << a + b;
+   }
+   if (op=='-'){
+     cout << a - b;
+   }
+   if (op=='*'){
+     cout << a * b;
+   }
+   if (op=='/'){
+     cout << a / b;
+   }
+   cout << endl;
+ }
