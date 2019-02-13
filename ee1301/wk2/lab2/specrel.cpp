24ae55fefee31a013e857b16123d6914f8335f07
RossTheRoss <msattr@gmail.com>
2019-02-13 19:05:44 +0000

    Hurry

--- ee1301/wk2/lab2/specrel.cpp
+++ ee1301/wk2/lab2/specrel.cpp
@@ -1,13 +1,25 @@
+/*
+Date: 13 Feb 2019
+Name: Matthew Strapp
+Student ID number: 5449340
+Course number: EE1301
+Term: Spring 2019
+Lab/assignment number: W-Up 1
+Short Program Description: Special Relativity equation tool
+*/
+
 #include <iostream>
-#inlcude <cmath>
+#include <cmath>
 using namespace std;
 
 int main () {
 	
-	double L,v,c,Lr;
+	double L,v,c;
 	c=3e+10;
 	cout << "Enter L: "; cin >> L;
-	cout << "Enter V: "; cin << v;
-	
-	
+	cout << "Enter V: "; cin >> v;
+	cout << "Relative Length = " <<
+	L * sqrt(1-(v*v)/(c*c)) //Relativistic Equation
+	<< endl;
+	return 0;
 }
