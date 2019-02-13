ca4e867f9fac8deec596283232c0a0fbd0e67d88
Matthew Strapp <msattr@gmail.com>
2019-02-13 10:43:47 -0600

    Do a stuff

---
+++ ee1301/wk2/lab2/specrel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#inlcude <cmath>
+using namespace std;
+
+int main () {
+	
+	double L,v,c,Lr;
+	c=3e+10;
+	cout << "Enter L: "; cin >> L;
+	cout << "Enter V: "; cin << v;
+	
+	
+}
