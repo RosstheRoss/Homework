210638883d3645a98c8f7d1dbb3c63e78ff87fc6
RossTheRoss <msattr@gmail.com>
2019-02-23 18:40:40 -0600

    Do easier HW bits

---
+++ ee1301/wk3/hw3_directory/strap012_HW3B.cpp
@@ -0,0 +1,40 @@
+/*
+27 Feb 2019
+Matthew Strapp
+5449340
+EE1301
+Spring 2019
+Homework 3A
+One-armed Bandit Simulator
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    char character;
+    bool isNotAlphaNumeric=false;
+    while (!isNotAlphaNumeric) {
+    cout << "Enter a single digit or an alphabetic character: ";
+    cin >> character;
+    cout << "You entered " << character;
+    if (character >= '0' && character <= '9')
+    {
+        cout <<  ", which is a number." << endl;
+    } else {
+        if (character>='a' && character<='z') {
+            cout << ", which is a lower case letter." << endl;
+        } else {
+            if (character>= 'A' && character<='Z') {
+                cout << ", which is an upper case letter." << endl;
+            } else {
+                isNotAlphaNumeric=true;
+                cout << ", which is not a letter or a number." << endl;
+            }
+        }
+        }
+    }
+}
---
+++ ee1301/wk3/hw3_directory/strap012_HW3C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+char swapCase(char s) {
+    
+}
+
+int main()
+{
+
+}
