1890f33d93f6643c4491b0c6f10ff3ae7ac7bc47
RossTheRoss <msattr@gmail.com>
2019-02-24 11:56:45 -0600

    Finish 3C

--- ee1301/wk3/hw3_directory/strap012_HW3C.cpp
+++ ee1301/wk3/hw3_directory/strap012_HW3C.cpp
@@ -4,10 +4,36 @@
 #include <iomanip>
 using namespace std;
 char swapCase(char s) {
-    
+    char New;
+    cout << "You entered " << s;
+    if (s >= 'a' && s <= 'z')
+    {
+        New= s - 32;
+        cout << ", I respond with " << New;
+    }
+    else
+    {
+        if (s >= 'A' && s <= 'Z')
+        {
+            New = s + 32;
+            cout << ", I respond with " << New;
+        }
+        else
+        {
+            cout << ", which is not a valid character";
+        }
+    }
+    cout << "." << endl;
 }
 
 int main()
 {
-
+    char character;
+    do {
+        cout << "Please enter a character that is an ASCII letter in the range [A-Za-z]: ";
+        cin >> character;
+        if (character!='@') {
+            swapCase(character);
+        }
+    } while (character!='@');
 }
