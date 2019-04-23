207befebb4932a4cb6a6497d744ce24b10bdabec
RossTheRoss <msattr@gmail.com>
2019-04-23 12:15:34 -0500

    More lab

--- ee1301/wk6/lab5/momentum.cpp
+++ ee1301/wk6/lab5/momentum.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-double* momentum (double velocity[3], double mass);
+double momentum (double velocity, double mass);
 
 int main() {
   double velocity[3];
@@ -9,14 +9,19 @@ int main() {
   std::cin >> velocity[0] >> velocity[1] >> velocity [2];
   std::cout << "Please enter mass [kg]: ";
   std::cin >> mass;
-  momentum(&velocity[3], mass);
-}
-
-double* momentum(double velocity[3], double mass) {
   double* vector;
   vector = new double[3];
-  for (int i=0; i<2; i++) {
-    vector[i]=velocity[i]*mass;
+  std::cout << "Momentum: <";
+  for (int i=0; i<=2; i++) {
+    vector[i]=momentum(velocity[i], mass);
+    std::cout << vector[i];
+    if ((i<2))
+      std::cout << ",";
   }
-  return vector;
+  delete[] vector;
+  std::cout << ">" << std::endl;
+}
+
+double momentum(double velocity, double mass) {
+  return velocity * mass;
 }
