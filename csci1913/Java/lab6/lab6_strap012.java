4e15412bd6bbc53bf60dadd1587fff93de2c83b7
RossTheRoss <mstrapp@protonmail.com>
2019-10-16 14:56:08 -0500

    Finish lab

---
+++ csci1913/Java/lab6/lab6_strap012.java
@@ -0,0 +1,19 @@
+package lab6;
+
+class Rectangle extends Polygon {
+    public Rectangle(int l, int w) {
+        super(4, l, w, l, w);
+    }
+    public int area() {
+        return this.side(0)*this.side(1);
+    }
+}
+
+class Square extends Polygon {
+    public Square(int l) {
+        super(4, l, l, l, l);
+    }
+    public int area() {
+        return this.side(0)*this.side(1);
+    }
+}
