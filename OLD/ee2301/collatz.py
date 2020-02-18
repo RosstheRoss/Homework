e71e5db8bf49cce950fe2c22fd7e2c125a25d00d
RossTheRoss <msattr@gmail.com>
2020-02-18 10:07:23 -0600

    Fix Collatz problem

--- OLD/ee2301/collatz.py
+++ OLD/ee2301/collatz.py
@@ -1,21 +1,21 @@
-def collatzCount(number):
-    for n in range (1,number+1):
+def collatzCount(k):
+    for n in range (1,k+1):
         collatzConjecture(n)
-        #print("\n")
+        print("\n")
 
 def collatzConjecture(n):
-    #print(n, end = ' ')
+    print(n, end = ' ')
     if n is 1:
         return 1
-    else:
+    elif n > 1:
         if n%2:
             return collatzConjecture(int((n*3)+1))
         else:
             return collatzConjecture(int(n/2))
-        raise AssertionError
+    raise AssertionError
 
 
 try:
-    collatzCount(10000)
+    collatzCount(100000)
 except AssertionError:
     print("The Collatz Conjecture is false!")
