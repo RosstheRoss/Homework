42af1c9c9e3bedcc362ee86ee1d7cc8a887e2ebe
RossTheRoss <msattr@gmail.com>
2020-03-02 12:10:27 -0600

    Thanks Jeremiah

---
+++ csci4041/hw2prob2.py
@@ -0,0 +1,16 @@
+
+# Watch the value of K!!!
+# - Jeremiah
+def making_sets(n, k, e, s):
+    if k == 0:
+        print(s)
+    else:
+        for e_i in range(e, n + 1):
+            s = set.union(s, {e_i})
+            making_sets(n, k - 1, e_i + 1, s)
+            s = set.difference(s, {e_i})
+
+def make_sets(n, k):
+    making_sets(n, k, 1, set())
+
+make_sets(4,3)
