e5052471897babac29849a2098b19a861e1eeeef
RossTheRoss <mstrapp@protonmail.com>
2021-03-22 12:17:32 -0500

    almost delete file

--- csci4131/hw5/strap012_hw5/createServer.js
+++ csci4131/hw5/strap012_hw5/createServer.js
@@ -9,8 +9,8 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   if(req.url === '/'){
-    res.writeHead(302, { 'Location': 'contacts.html' });
-    return res.end("302 Temporary Redirect");
+    res.writeHead(301, { 'Location': 'index.html' });
+    return res.end("301 Permanent Redirect");
   }
   else if (req.url === '/index.html') {
     indexPage(req, res, '/index.html');
@@ -28,14 +28,15 @@ http.createServer(function (req, res) {
     jsonSock(req, res, 'contacts.json')
   }
   else if (req.url === "/postContactEntry") {
-    
+    POST(req, res);
+    res.writeHead(302, { 'Location': 'contacts.html' });
+    return res.end("302 Temporary Redirect");
   }
   else{
     res.writeHead(404, {'Content-Type': 'text/html'});
     return res.end("404 Not Found");
   }
 }).listen(port);
-
 
 function indexPage(req, res, file) {
   fs.readFile('client' + file, function(err, html) {
@@ -61,3 +62,30 @@ function jsonSock(req, res, file) {
   });
 }
 
+function POST(req, res) {
+  var reqBody = '';
+  // server starts receiving the form data
+  req.on('data', function(data) {
+    reqBody+= data;
+  });// server has received all the form data
+  req.on('end', function() {
+    var jsonO = {}
+    var postObj = qs.parse(reqBody);
+    jsonO["name"] = postObj.name;
+    jsonO["category"] = postObj.category;
+    jsonO["location"] = postObj.location;
+    jsonO["contact"] = postObj.contact;
+    jsonO["email"] = postObj.email;
+    jsonO["website_name"] = postObj.website_name;
+    jsonO["website_url"] = postObj.website_url;
+    let read = fs.readFile('./contacts.json', 'utf8', function(err, data) {
+      var file = JSON.parse(data);
+      file.contacts.push(jsonO);
+      var string = JSON.stringify(file);
+      fs.writeFile('./contacts.json', string, function(err, data) {
+        console.log(string);
+      });
+    });
+    
+  });
+}
